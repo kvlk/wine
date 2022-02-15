13ce3b3e405655638e6277d5716a6c3a66c1be36
Konstantin Volkov <wallcaw@ya.ru>
2022-02-15 14:23:53 +0300

    24 columns grid in place of default tailwind's 12 columns

--- src/components/OProductDetails.vue
+++ src/components/OProductDetails.vue
@@ -31,41 +31,46 @@ import MInfoDescription from './MInfoDescription.vue'
 <style lang="scss">
 .o-product-details {
   @apply 
-    xl:container xl:mx-auto
-    flex flex-col lg:grid lg:grid-cols-12 lg:grid-rows-none 
+    2xl:container xl:mx-auto
+    flex flex-col lg:grid lg:grid-cols-24 lg:grid-rows-none 
     lg:px-7 pb-10 pt-0 lg:pt-10 
     bg-gray-100 lg:bg-transparent;
 
   .a-image {
     @apply 
-      lg:col-start-1 lg:col-end-8 lg:row-start-2 lg:row-end-5 
-      xl:col-start-1 xl:col-end-6; 
+      lg:col-start-1 lg:col-end-16 lg:row-start-2 lg:row-end-5 
+      xl:col-start-1 xl:col-end-10 
+      2xl:col-end-9;
   }
   .m-info-name {
     @apply 
       flex flex-col order-1 lg:block 
-      lg:col-start-8 lg:col-end-13 
-      xl:col-start-6 xl:col-end-10 xl:row-start-2 
+      lg:col-start-16 lg:col-end-25 
+      xl:col-start-10 xl:col-end-18 xl:row-start-2 
+      2xl:col-start-9 2xl:col-end-19 
       lg:ml-10 xl:ml-8 xl:mr-6;
   }
   .m-info-description {
     @apply 
       order-4 
-      lg:col-start-8 lg:col-end-13 
-      xl:col-start-6 xl:col-end-10 xl:row-start-3 
+      lg:col-start-16 lg:col-end-25 
+      xl:col-start-10 xl:col-end-18 xl:row-start-3 
+      2xl:col-start-9 2xl:col-end-19 
       lg:ml-10 xl:ml-8 xl:mr-6;
   }
   .m-offer-box {
     @apply 
       flex flex-col order-3 xl:block
-      lg:col-start-8 lg:col-end-13 
-      xl:col-start-10 xl:col-end-13 xl:row-start-2 xl:row-end-5 
+      lg:col-start-16 lg:col-end-25 
+      xl:col-start-18 xl:col-end-25 xl:row-start-2 xl:row-end-5 
+      2xl:col-start-19
       lg:ml-10 xl:mb-auto;
   }
   .a-badge-code {
     @apply 
-      lg:col-start-8 lg:col-end-13 
-      xl:col-start-10 xl:col-end-13 xl:row-start-1 xl:items-start 
+      lg:col-start-16 lg:col-end-25 
+      xl:col-start-18 xl:col-end-25 xl:row-start-1 xl:items-start 
+      2xl:col-start-19 
       lg:ml-10;
   }
 }
